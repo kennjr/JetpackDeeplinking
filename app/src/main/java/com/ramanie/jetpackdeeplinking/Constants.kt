493253784243e0a8b07b0de575e8937a0d1d2993
kenedy <kenedy@myhechar.pro>
2022-11-14 11:35:07 +0300

    created a file where all constants e.g keys will be stored

---
+++ app/src/main/java/com/ramanie/jetpackdeeplinking/Constants.kt
@@ -0,0 +1,10 @@
+package com.ramanie.jetpackdeeplinking
+
+enum class Destinations(val destination: String){
+    HOME_SCREEN("home_screen"),
+    DETAIL_SCREEN("detail_screen")
+}
+
+enum class ArgumentKeys(val key: String){
+    ARG_ID("id")
+}
